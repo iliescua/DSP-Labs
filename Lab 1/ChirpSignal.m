40072bcb1f73221b7128a749998cc7236575f69b
Iliescu <iliescua@ad.msoe.edu>
2019-12-13 13:24:06 -0600

    Cleaned up the code and added comments, as well as getting the entire lab working. I still need to answer the questions

--- Lab 1/ChirpSignal.m
+++ Lab 1/ChirpSignal.m
@@ -6,7 +6,9 @@
 S=load('chirp'); 
 x=S.y; fs=S.Fs;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Original Graphs (Unmodified)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure
 %sound(x);
 %Discrete Time Graph
@@ -15,7 +17,6 @@ stem(1:length(x), x, 'Color', 'k')
 xlabel('n','FontSize',14,'FontAngle','italic')
 ylabel('x[n]','FontSize',14,'FontAngle','italic')
 title('Discrete Time Signal','FontSize',16)
-
 %Continuous Time Graph
 subplot(212)
 plot((1:length(x))/fs, x, 'Color', 'k')
@@ -23,8 +24,9 @@ xlabel('t','FontSize',14,'FontAngle','italic')
 ylabel('x(t)','FontSize',14,'FontAngle','italic')
 title('Continuous Time Signal','FontSize',16)
 
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Amplitude Scaling Applied 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure
 c=100;
 y=c*x;
@@ -35,7 +37,6 @@ stem(1:length(y), y, 'Color', 'k')
 xlabel('n','FontSize',14,'FontAngle','italic')
 ylabel('x[n]','FontSize',14,'FontAngle','italic')
 title('Discrete Time Signal (Amplitude Scaling)','FontSize',16)
-
 %Continuous Time Graph
 subplot(212)
 plot((1:length(y))/fs, y, 'Color', 'k')
@@ -43,8 +44,9 @@ xlabel('t','FontSize',14,'FontAngle','italic')
 ylabel('x(t)','FontSize',14,'FontAngle','italic')
 title('Continuous Time Signal(Amplitude Scaling)','FontSize',16)
 
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Time Reversal Applied 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure
 y=flipud(x);
 %sound(y);
@@ -54,7 +56,6 @@ stem(1:length(y), y, 'Color', 'k')
 xlabel('n','FontSize',14,'FontAngle','italic')
 ylabel('x[n]','FontSize',14,'FontAngle','italic')
 title('Discrete Time Signal (Time Reversal)','FontSize',16)
-
 %Continuous Time Graph
 subplot(212)
 plot((1:length(y))/fs, y, 'Color', 'k')
@@ -62,8 +63,9 @@ xlabel('t','FontSize',14,'FontAngle','italic')
 ylabel('x(t)','FontSize',14,'FontAngle','italic')
 title('Continuous Time Signal(Time Reversal)','FontSize',16)
 
-
-%Time Scaling Applied 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Time Scaling Applied (Upsample)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure
 c=2;
 y=upsample(x,c);
@@ -73,17 +75,37 @@ subplot(211)
 stem(1:length(y), y, 'Color', 'k')
 xlabel('n','FontSize',14,'FontAngle','italic')
 ylabel('x[n]','FontSize',14,'FontAngle','italic')
-title('Discrete Time Signal (Time Scaling)','FontSize',16)
-
+title('Discrete Time Signal (Time Scaling Upsample)','FontSize',16)
 %Continuous Time Graph
 subplot(212)
 plot((1:length(y))/fs, y, 'Color', 'k')
 xlabel('t','FontSize',14,'FontAngle','italic')
 ylabel('x(t)','FontSize',14,'FontAngle','italic')
-title('Continuous Time Signal(Time Scaling)','FontSize',16)
+title('Continuous Time Signal(Time Scaling Upsample)','FontSize',16)
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Time Scaling Applied Downsample
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure
+c=2;
+y=downsample(x,c);
+sound(y);
+%Discrete Time Graph
+subplot(211)
+stem(1:length(y), y, 'Color', 'k')
+xlabel('n','FontSize',14,'FontAngle','italic')
+ylabel('x[n]','FontSize',14,'FontAngle','italic')
+title('Discrete Time Signal (Time Scaling Downsample)','FontSize',16)
+%Continuous Time Graph
+subplot(212)
+plot((1:length(y))/fs, y, 'Color', 'k')
+xlabel('t','FontSize',14,'FontAngle','italic')
+ylabel('x(t)','FontSize',14,'FontAngle','italic')
+title('Continuous Time Signal(Time Scaling Downsample)','FontSize',16)
 
-%Time Shifting Applied 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Time Shifting Applied
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure
 c=8192;
 y=cat(1,zeros(c,1),x);
@@ -94,7 +116,6 @@ stem(1:length(y), y, 'Color', 'k')
 xlabel('n','FontSize',14,'FontAngle','italic')
 ylabel('x[n]','FontSize',14,'FontAngle','italic')
 title('Discrete Time Signal (Time Shifting)','FontSize',16)
-
 %Continuous Time Graph
 subplot(212)
 plot((1:length(y))/fs, y, 'Color', 'k')
@@ -102,8 +123,9 @@ xlabel('t','FontSize',14,'FontAngle','italic')
 ylabel('x(t)','FontSize',14,'FontAngle','italic')
 title('Continuous Time Signal(Time Shifting)','FontSize',16)
 
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Amplitude Shifting Applied 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure
 c=1;
 y=x+c;
@@ -114,7 +136,6 @@ stem(1:length(y), y, 'Color', 'k')
 xlabel('n','FontSize',14,'FontAngle','italic')
 ylabel('x[n]','FontSize',14,'FontAngle','italic')
 title('Discrete Time Signal (Amplitude Shifting)','FontSize',16)
-
 %Continuous Time Graph
 subplot(212)
 plot((1:length(y))/fs, y, 'Color', 'k')
